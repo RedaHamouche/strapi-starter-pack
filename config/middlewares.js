256a5c682b873aba2c6b70a193e9eb8544d084d0
RedaHamouche <reda.hamouche@outlook.com>
2023-07-05 16:03:56 +0200

    postgre added, production server added

--- config/middlewares.js
+++ config/middlewares.js
@@ -4,10 +4,9 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:3000'],
+      origin: ['http://localhost:3000', 'http://localhost:1337'],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       allowedHeaders: ['Content-Type', 'Authorization'],
-      credentials: true,
     },
   },
   'strapi::poweredBy',
